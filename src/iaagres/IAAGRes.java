69df4e56d8d76124e47394f01db1401018ed4507
Deportes <Deportes@Atlantis-PC>
2015-12-05 20:02:28 -0500

    Creación.

---
+++ src/iaagres/IAAGRes.java
@@ -0,0 +1,16 @@
+package iaagres;
+
+/**
+ *
+ * @author Deportes
+ */
+public class IAAGRes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
